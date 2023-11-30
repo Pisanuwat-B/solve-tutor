4a9371507b91c154367825b530913b7af48664d7
Puzzorn <pongpathn@gmail.com>
2023-11-30 22:20:37 +0700

    -fix: responsive

--- lib/feature/live_classroom/utils/responsive.dart
+++ lib/feature/live_classroom/utils/responsive.dart
@@ -4,51 +4,44 @@ import 'package:flutter/material.dart';
 
 class Responsive extends StatelessWidget {
   final Widget mobile;
-  final Widget? tablet;
+  final Widget tablet;
   final Widget desktop;
+
+  static const double tabletBreakpoint = 768;
+  static const double desktopBreakpoint = 1100;
+  static const double tabletLandscapeBreakpoint = 1024;
 
   const Responsive({
     Key? key,
     required this.mobile,
-    this.tablet,
+    required this.tablet,
     required this.desktop,
   }) : super(key: key);
 
-  // This isMobile, isTablet, isDesktop help us later
   static bool isMobile(BuildContext context) =>
-      MediaQuery.of(context).size.width < 650 &&
-      MediaQuery.of(context).orientation == Orientation.portrait;
+      MediaQuery.of(context).size.width < tabletBreakpoint;
 
   static bool isMobileLandscape(BuildContext context) =>
-      MediaQuery.of(context).size.width < 850 &&
-      MediaQuery.of(context).orientation == Orientation.landscape;
+      MediaQuery.of(context).size.width >= tabletBreakpoint &&
+          MediaQuery.of(context).size.width < tabletLandscapeBreakpoint &&
+          MediaQuery.of(context).orientation == Orientation.landscape;
 
   static bool isTablet(BuildContext context) =>
-      MediaQuery.of(context).size.width < 1100 &&
-      MediaQuery.of(context).size.width >= 650 &&
-      MediaQuery.of(context).orientation == Orientation.portrait;
+      MediaQuery.of(context).size.width >= tabletBreakpoint &&
+          MediaQuery.of(context).size.width < desktopBreakpoint &&
+          MediaQuery.of(context).orientation == Orientation.portrait;
 
   static bool isDesktop(BuildContext context) =>
-      MediaQuery.of(context).size.width >= 1100;
+      MediaQuery.of(context).size.width >= desktopBreakpoint &&
+          MediaQuery.of(context).orientation != Orientation.portrait;
 
   @override
   Widget build(BuildContext context) {
-    final Size _size = MediaQuery.of(context).size;
-    // If our width is more than 1100 then we consider it a desktop
-    log('size = $_size');
-
-    if (_size.width >= 1100) {
-      log("Size Desktop");
+    if (isDesktop(context)) {
       return desktop;
-    }
-    // If width it less then 1100 and more then 850 we consider it as tablet
-    else if (_size.width >= 650 && tablet != null) {
-      log("Size Tablet");
-      return tablet!;
-    }
-    // Or less then that we called it mobile
-    else {
-      log("Size Mobile");
+    } else if (isTablet(context)) {
+      return tablet;
+    } else {
       return mobile;
     }
   }
